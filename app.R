4d33370e0f916b08624ae09fdbb14f19dfe9edf2
Eric <enshapir@andrew.cmu.edu>
2018-09-12 09:49:13 -0400

    created single file for homework
    Completed my data wrangling by combining 5 open data files

--- app.R
+++ app.R
@@ -1,13 +1,33 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# ----------------------------
+# HW2 - enshapir
+# ----------------------------
 
 library(shiny)
+library(tidyverse)
+
+#quarries the system for the directory of this file and then sets the directory of the file. 
+current_file_path <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(current_file_path)
+
+#data from Western PA 
+
+#file path names of the data used
+JailFile1 <- file.path('open_data_files', '8fb43d9a-9fb9-431d-93d2-9be7330dc846.csv', fsep = .Platform$file.sep)
+JailFile2 <- file.path('open_data_files', '35b19857-e653-4801-a4ba-022365341ce9.csv', fsep = .Platform$file.sep)
+JailFile3 <- file.path('open_data_files', '85e0c576-41af-403d-a994-a27834b15157.csv', fsep = .Platform$file.sep)
+JailFile4 <- file.path('open_data_files', 'aa4cae88-0831-46ec-8853-7707202f88f4.csv', fsep = .Platform$file.sep)
+JailFile5 <- file.path('open_data_files', 'fd3b3cae-355c-46ca-a505-b1f0672153c7-5.csv', fsep = .Platform$file.sep)
+
+#read in the survey data and data dicitonary 
+Jail_Raw1 <- read.csv(file = JailFile1, header = TRUE, sep = ",")
+Jail_Raw2 <- read.csv(file = JailFile2, header = TRUE, sep = ",")
+Jail_Raw3 <- read.csv(file = JailFile3, header = TRUE, sep = ",")
+Jail_Raw4 <- read.csv(file = JailFile4, header = TRUE, sep = ",")
+Jail_Raw5 <- read.csv(file = JailFile5, header = TRUE, sep = ",")
+
+#combine all the files
+JailTotal <- bind_rows(Jail_Raw1, Jail_Raw2, Jail_Raw3, Jail_Raw4, Jail_Raw5)
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
