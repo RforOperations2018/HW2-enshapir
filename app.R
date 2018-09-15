4f77f8076fc0aeca7e3127c818374fa66d2cc1d8
Eric <enshapir@andrew.cmu.edu>
2018-09-15 02:15:20 -0400

    final
    complete

--- app.R
+++ app.R
@@ -21,7 +21,7 @@ JailFile3 <- file.path('open_data_files', '85e0c576-41af-403d-a994-a27834b15157.
 JailFile4 <- file.path('open_data_files', 'aa4cae88-0831-46ec-8853-7707202f88f4.csv', fsep = .Platform$file.sep)
 JailFile5 <- file.path('open_data_files', 'fd3b3cae-355c-46ca-a505-b1f0672153c7-5.csv', fsep = .Platform$file.sep)
 
-#read in the survey data and data dicitonary 
+#read in the data
 Jail_Raw1 <- read.csv(file = JailFile1, header = TRUE, sep = ",")
 Jail_Raw2 <- read.csv(file = JailFile2, header = TRUE, sep = ",")
 Jail_Raw3 <- read.csv(file = JailFile3, header = TRUE, sep = ",")
@@ -34,28 +34,35 @@ JailTotal <- bind_rows(Jail_Raw1, Jail_Raw2, Jail_Raw3, Jail_Raw4, Jail_Raw5)
 #removing row with age value of -1 which is not possible
 JailTotal <- JailTotal %>% filter(Current.Age != -1)
 
+JailTotal$Race <- plyr::revalue(JailTotal$Race, replace = c("A"="ASIAN", 
+                                                      "B"="BLACK",
+                                                      "H"="HISPANIC",
+                                                      "I"="NATIVE AMERICAN",
+                                                      "U"="UNKNOWN",
+                                                      "W"="WHITE"))
 
-#looing at average age by gender
+
+#count by Date, Race, Current Age grouping
 AgeRaceByDay <- JailTotal %>% 
   filter(!is.na(Current.Age)) %>%  
   count(Date, Race, Current.Age) %>% 
   as.data.frame()
-
+#date conversion 
 AgeRaceByDay$Date <- as.Date(AgeRaceByDay$Date, "%m/%d/%Y")
 
-#for looking at race/age brekdown of under 18 population
+#Count by grouping Race, Current Age for under 18 populaton
 RaceByAge<- JailTotal %>% 
   filter(Current.Age>10&Current.Age<18) %>%  
   count(Race, Current.Age) %>% as.data.frame()
 
 
-#for looking at race/age brekdown of under 18 population
+#grouping by Race, Gender and getting mean age
 RaceAgeGender<- JailTotal %>% 
   filter(!is.na(Current.Age)) %>%  
-  group_by(Race, Current.Age, Gender) %>% 
+  group_by(Race, Gender) %>% 
   summarise(MeanAge = mean(Current.Age))
 
-# Define UI for application that draws a histogram
+# Define UI
 ui <- fluidPage(
   
   navlistPanel("State of Allegheny County Jail", 
@@ -68,7 +75,7 @@ ui <- fluidPage(
                                       choices = sort(unique(RaceAgeGender$Race)),
                                       multiple = T,
                                       selectize = T,
-                                      selected = c("A", "B")),
+                                      selected = c("ASIAN", "BLACK")),
                           actionButton("reset", "Reset Filters", icon = icon("refresh"))
                         ),
                         # Output plot
@@ -155,39 +162,42 @@ server <- function(input, output, session = session) {
   swInput4 <- reactive({
     JailTotal.server <- JailTotal
   })
-
+#outplut for plot 1 showing the average age for each gender by race
   output$Raceplot <- renderPlotly({
     dat <- swInput()
     ggplotly(
       ggplot(data = dat, aes(x = Gender, y = MeanAge, color = Race, text = paste0("<br>Race: ", Race,
                                                                                   "<br>Gender: ", Gender,
-                                                                                  "<br>Mean Age: ", MeanAge))) + 
-        geom_jitter()+
-        guides(color = FALSE)
-      , tooltip = "text")
+                                                                                  "<br>Mean Age: ", round(MeanAge, 2)))) + 
+        geom_point()+
+        guides(color = FALSE)+
+        labs(x="Gender", y="Average Age", title = "Average Age of Gender by Race"),
+        tooltip = "text")
   })  
-  
+  #output for plot 2 showing the number of individuals by race between specific ages
     output$Under18plot <- renderPlotly({
       dat2 <- swInput2()
       ggplotly(
       ggplot(data = dat2, aes(x = Race, y = n, fill = Race, text = paste0("<br>Race: ", Race,
                                                                          "<br>Count: ", n))) + 
-        geom_bar(stat = "identity"),
+        geom_bar(stat = "identity")+
+        labs(x="Race", y="Count of Individuals", title = "Count of Individuals by Race between Specified Ages"),
       tooltip = "text")
   })  
-  
+  #output of plot 3 showing the distributoin of of each race by age on a specific day
     output$DateRacePlot <- renderPlotly({
       dat3 <- swInput3()
       ggplotly(
         ggplot(dat3, aes(x=Current.Age, y= n, fill=Race)) +
                  geom_bar(stat="identity")+
-                 facet_wrap(~Race)
+                 facet_wrap(~Race)+
+          labs(x="Age", y="Count of Individuals", title = "Distribution of Age by Race on a Specific Day")
         )
     })  
     
-    
+    #reset for plot 1
   observeEvent(input$reset, {
-    updateSelectInput(session, "SelectedRace", selected = c("A", "B"))
+    updateSelectInput(session, "SelectedRace", selected = c("ASIAN", "BLACK"))
     showNotification("You have successfully reset to show all races", type = "message")
   })
   
